4be8ea8aa4a885ebb9e70cbe19e63f1c6cf67c17
Gustav Utke Kauman <gustav.kauman@gmail.com>
2022-12-05 22:41:01 +0100

    Version that can start editorservices

---
+++ lua/al-neovim/al-lsp.lua
@@ -0,0 +1,77 @@
+local al_lsps = {}
+local al_filetypes = {
+    ["al"] = true;
+}
+
+local function path_join(...)
+	return table.concat(vim.tbl_flatten {...}, path_sep)
+end
+
+local path_sep = vim.loop.os_uname().sysname == "Windows" and "\\" or "/"
+
+local function dirname(filepath)
+	local is_changed = false
+	local result = filepath:gsub(path_sep.."([^"..path_sep.."]+)$", function()
+		is_changed = true
+    	return ""
+	end)
+  	return result, is_changed
+end
+
+local al_lsp_config = {
+    name = "al";
+    cmd = { path_join(os.getenv("HOME"),
+        "/.vscode/extensions/ms-dynamics-smb.al-9.5.674382/bin/darwin/",
+        "Microsoft.Dynamics.Nav.EditorServices.Host")
+    };
+}
+
+local function buffer_find_root_dir(bufnr, is_root_path)
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+    if vim.fn.filereadable(bufname) == 0 then
+        return nil
+    end
+
+    local dir = bufname
+
+    for _ = 1, 100 do
+        local did_change
+        dir, did_change = dirname(dir)
+        if is_root_path(dir, bufname) then
+            return dir, bufname
+        end
+        if not did_change then
+            return nil
+        end
+    end
+end
+
+function check_start_al_lsp()
+    local bufnr = vim.api.nvim_get_current_buf()
+    if not al_filetypes[vim.api.nvim_buf_get_option(bufnr, 'filetype')] then
+        return
+    end
+    
+    local root_dir = buffer_find_root_dir(bufnr, function(dir)
+        return vim.fn.filereadable(path_join(dir, 'app.json')) == 1
+    end)
+
+    if not root_dir then return end
+
+    local client_id = al_lsps[root_dir]
+    if not client_id then
+        local new_conf = vim.tbl_extend("error", al_lsp_config, {
+            root_dir = root_dir;
+        })
+        client_id = vim.lsp.start_client(new_conf)
+        al_lsps[root_dir] = client_id
+    end
+    
+    vim.lsp.buf_attach_client(bufnr, client_id)
+end
+
+return {
+	al_neovim = check_start_al_lsp
+}
+
---
+++ plugin/al-neovim.vim
@@ -0,0 +1,16 @@
+" Title: AL Neovim
+" Description: A plugin that allows your Neovim installation to interact with
+" the AL language server.
+" Last Change: 3 December 2022
+" Maintainer: Gustav Utke Kauman <hello@kauman.dev>
+
+if exists("g:loaded_alneovim")
+	finish " avoid loading plugin twice
+endif
+let g:loaded_alneovim = 1
+
+" Set filetype for *.al files
+au BufReadPost *.al set filetype=al
+
+command! ALNeovim lua require("al-neovim.al-lsp").al_neovim()
+
